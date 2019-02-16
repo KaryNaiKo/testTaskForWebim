1318ea753a6e810ee6ae0c593a2c9c1c61cfc761
KaryNaiKo <firamar00@gmail.com>
2019-02-16 20:35:15 +0300

    fix OAuthUtil

--- src/main/java/com/github/karynaiko/web/OAuthUtil.java
+++ src/main/java/com/github/karynaiko/web/OAuthUtil.java
@@ -6,6 +6,6 @@ public class OAuthUtil {
     static final String METHOD_URL = "https://api.vk.com/method/";
     static final String CLIENT_ID = "6856264";
     static final String CLIENT_SECRET = "jvyBTez2uOybuZB5RzRq";
-    static final String REDIRECT_URI = "http://localhost:8080/oauth/vk/callback";
+    static final String REDIRECT_URI = "http://testtaskforwebim.herokuapp.com/oauth/vk/callback";
     static final String VERSION = "5.92";
 }
